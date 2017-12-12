e057215f55285ee80faafaee5e7b5e9b87b8e685
Thibaut-Comte <thibaut.comte.1@gmail.com>
2017-12-12 16:00:32 +0100

    links button menu

--- reaction/app/src/main/java/com/jonathan/reaction/Menu.java
+++ reaction/app/src/main/java/com/jonathan/reaction/Menu.java
@@ -9,11 +9,52 @@ import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
 
+    Button settings, speed, stamina, score, average;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        settings = findViewById(R.id.btnSettings);
+        score = findViewById(R.id.btnScore);
+        average = findViewById(R.id.btnAverage);
+        speed = findViewById(R.id.btnSpeed);
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(Menu.this, Settings.class);
+//                startActivity(intent);
+            }
+        });
+
+        score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, Score.class);
+                startActivity(intent);
+            }
+        });
+
+        average.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(Menu.this, Average.class);
+//                startActivity(intent);
+            }
+        });
+
+        speed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(Menu.this, Speed.class);
+//                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
